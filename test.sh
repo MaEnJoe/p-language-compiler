3b8d83d9e7ff190375db85ec5d9d026143b90f81
Heron Yang <heron.yang.tw@gmail.com>
2014-03-26 14:06:21 +0800

    update test.sh

--- test.sh
+++ test.sh
@@ -13,12 +13,22 @@ public/general/general_1
 public/basic/identifier
 public/digit/scientific"
 
+COUNT=0
+
 rm -f $OUTPUT
 for f in $FILES
 do
     $EXEC $f > /tmp/t
     diff /tmp/t $f"_ans" >> $OUTPUT
     echo "---file:"$f" ends here" >> $OUTPUT
+    COUNT=$((COUNT+1))
 done
 
+lines=`wc -l $OUTPUT | cut -f1 -d' '`
 echo "output save in "$OUTPUT
+
+if [ $lines == $COUNT ]; then
+    echo "Pass!"
+else
+    echo "Failed!"
+fi
